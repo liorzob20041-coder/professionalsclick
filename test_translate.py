d888ffb64f4a849b607f2cb2e2fee226d73f046f
liorzob20041-coder <liorzob20041@gmail.com>
2025-09-27 00:22:20 +0300

    Clean repo: remove old root folders; keep balei-miktzoa-site

--- test_translate.py
+++
@@ -1,8 +0,0 @@
-from deep_translator import GoogleTranslator
-
-text = "היי בדיקת תרגום"
-translated_en = GoogleTranslator(source='iw', target='en').translate(text)
-translated_ru = GoogleTranslator(source='iw', target='ru').translate(text)
-
-print("EN:", translated_en)
-print("RU:", translated_ru)
